e4c392ebce1cbc164696e74bbb2b11032a918f38
Ashwin Sathian <ashwinsathyan19@gmail.com>
2024-09-15 22:58:17 +0530

    Add Inventory View

--- src/app/inventory/components/view-inventory/view-inventory.component.ts
+++ src/app/inventory/components/view-inventory/view-inventory.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Inventory } from '../../../../interfaces/inventory.interface';
 
 @Component({
   selector: 'app-view-inventory',
   templateUrl: './view-inventory.component.html',
-  styleUrl: './view-inventory.component.css'
+  styleUrl: './view-inventory.component.css',
 })
-export class ViewInventoryComponent {
+export class ViewInventoryComponent implements OnInit {
+  inventory!: Inventory;
 
+  constructor(private _route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    if (this._route.snapshot.data?.['inventory']?.id) {
+      this.inventory = JSON.parse(
+        JSON.stringify(this._route.snapshot.data['inventory'])
+      );
+    }
+  }
 }
