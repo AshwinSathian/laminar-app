2b745f3e542b5bcfa7e06233d33d56b9f386b6f4
Ashwin Sathian <ashwinsathyan19@gmail.com>
2025-02-07 20:26:40 +0530

    Update WS connection init

--- src/app/services/orders-socket.service.ts
+++ src/app/services/orders-socket.service.ts
@@ -4,9 +4,20 @@ import { io, Socket } from 'socket.io-client';
 
 @Injectable()
 export class OrdersSocketService {
-  private socket: Socket = io(environment.apiUrl);
+  private socket: Socket;
 
-  constructor() {}
+  constructor() {
+    this.socket = io(environment.apiUrl, {
+      transports: ['websocket', 'polling'],
+      withCredentials: true,
+    });
+
+    this.socket.on('connect', () => console.log('Connected to WebSocket'));
+    this.socket.on('disconnect', () => console.warn('WebSocket disconnected'));
+    this.socket.on('connect_error', (err) =>
+      console.error('WebSocket error:', err)
+    );
+  }
 
   listenForOrderUpdates(callback: (data: any) => void) {
     this.socket.on('orderUpdated', callback);
